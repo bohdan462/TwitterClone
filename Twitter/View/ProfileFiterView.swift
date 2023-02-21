390601cbc910ba28b35a474d776167bd750214f5
Bohdan Tkachenko <techbohdan@gmail.com>
2023-02-20 19:20:41 -0600

    Added ProfileFilter

--- Twitter/View/ProfileFiterView.swift
+++ Twitter/View/ProfileFiterView.swift
@@ -7,16 +7,24 @@
 
 import UIKit
 
+private let reuseIdentifier = "ProfileFilterCell"
+
+protocol ProfileFilterViewDelegate: AnyObject {
+    func filterView(_ view: ProfileFilterView, didSelect indexPath: IndexPath)
+}
+
 class ProfileFilterView: UIView {
     
     // MARK: - Properties
     
+    weak var delegate: ProfileFilterViewDelegate?
+    
     lazy var collectionView: UICollectionView = {
-        let layout = UICollectionViewLayout()
+        let layout = UICollectionViewFlowLayout()
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
         cv.backgroundColor = .white
-//        cv.delegate = self
-//        cv.dataSource = self
+        cv.delegate = self
+        cv.dataSource = self
         return cv
     }()
     
@@ -24,7 +32,11 @@ class ProfileFilterView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .red
+        
+        collectionView.register(ProfileFilterCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        
+        addSubview(collectionView)
+        collectionView.addConstraintsToFillView(self)
     }
     
     required init?(coder: NSCoder) {
@@ -33,16 +45,36 @@ class ProfileFilterView: UIView {
     
 }
 
-//extension ProfileFilterView: UICollectionViewDelegate {
-//
-//}
-//
-//extension ProfileFilterView: UICollectionViewDataSource {
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return 3
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        <#code#>
-//    }
-//}
+
+// MARK: - UICollectionViewDelegate
+
+extension ProfileFilterView: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        delegate?.filterView(self, didSelect: indexPath)
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension ProfileFilterView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: frame.width / 3, height: frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension ProfileFilterView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ProfileFilterCell
+        return cell
+    }
+}
